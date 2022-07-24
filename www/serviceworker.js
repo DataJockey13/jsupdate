3e80307a6a08d9029c606ec73d961a372930e4da
DataJockey <github@blackserver.de>
2022-07-24 19:51:07 +0200

    sw ...

--- www/serviceworker.js
+++ www/serviceworker.js
@@ -12,21 +12,26 @@ const appMessage = (msg) => {
     broadcast.postMessage(msg);
 }
 
+self.addEventListener('activate', (event) =>  {
+    console.log('Claiming control');
+    return self.clients.claim();
+});
+
 this.addEventListener('install', event => {
     event.waitUntil(
         caches
             .open(cacheName)
             .then(cache => cache.addAll(routes))
     );
+    self.skipWaiting();
     console.log("installed");
 });
 
 this.addEventListener('fetch', event => {
     //console.log("serviceworker: fetch " + event.request.url); 
 
-    event.respondWith(
-        (async function() {
-            const cache = await caches.open(cacheName);
+    event.respondWith( async () => {
+        const cache = await caches.open(cacheName);
             const cachedFiles = await cache.match(event.request);
             if (cachedFiles) {
                 console.log("serviceworker cache response for " + event.request.url);
@@ -35,7 +40,7 @@ this.addEventListener('fetch', event => {
                 console.log("serviceworker network response for " + event.request.url);
                 return fetch(event.request);
             }
-        }())
+        });
     );      
 });
 
